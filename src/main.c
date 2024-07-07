ac8f30ce21c1dbf88fa8da8dbe08c4871e2d259d
devin-hale <devinwaynehale@gmail.com>
2024-07-07 15:43:54 -0500

    Added support for push button for Move_Left() instruction

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
-
 #include "stm32f103xb.h"
 #include "stm32f1xx_ll_bus.h"
 #include "stm32f1xx_ll_gpio.h"
 #include "stm32f1xx_ll_rcc.h"
-#include "stm32f1xx_ll_tim.h"
 #include "stm32f1xx_ll_system.h"
+#include "stm32f1xx_ll_tim.h"
 #include <stdint.h>
 
 void systick_init(void) {
@@ -16,78 +15,121 @@ void systick_init(void) {
 
 void delay(int ms) {
   for (int i = 0; i < ms; i++) {
-    while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
+    while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk))
+      ;
   }
 }
 
 void gpio_init(void) {
-	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOA);
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOA);
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOB);
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOC);
 
-    LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_0, LL_GPIO_MODE_ALTERNATE);
-    LL_GPIO_SetPinSpeed(GPIOA, LL_GPIO_PIN_0, LL_GPIO_SPEED_FREQ_HIGH);
-    LL_GPIO_SetPinOutputType(GPIOA, LL_GPIO_PIN_0, LL_GPIO_OUTPUT_PUSHPULL);
+  // Servo 1
+  LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_0, LL_GPIO_MODE_ALTERNATE);
+  LL_GPIO_SetPinSpeed(GPIOA, LL_GPIO_PIN_0, LL_GPIO_SPEED_FREQ_HIGH);
+  LL_GPIO_SetPinOutputType(GPIOA, LL_GPIO_PIN_0, LL_GPIO_OUTPUT_PUSHPULL);
 
-	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_1, LL_GPIO_MODE_ALTERNATE);
-    LL_GPIO_SetPinSpeed(GPIOA, LL_GPIO_PIN_1, LL_GPIO_SPEED_FREQ_HIGH);
-    LL_GPIO_SetPinOutputType(GPIOA, LL_GPIO_PIN_1, LL_GPIO_OUTPUT_PUSHPULL);
+  // Servo 2
+  LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_1, LL_GPIO_MODE_ALTERNATE);
+  LL_GPIO_SetPinSpeed(GPIOA, LL_GPIO_PIN_1, LL_GPIO_SPEED_FREQ_HIGH);
+  LL_GPIO_SetPinOutputType(GPIOA, LL_GPIO_PIN_1, LL_GPIO_OUTPUT_PUSHPULL);
+
+  // PC13
+  LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_13, LL_GPIO_MODE_OUTPUT);
+  LL_GPIO_SetPinSpeed(GPIOC, LL_GPIO_PIN_13, LL_GPIO_SPEED_FREQ_HIGH);
+  GPIOC->ODR |= GPIO_ODR_ODR13;
+
+  // PC14
+  LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_14, LL_GPIO_MODE_INPUT);
+  LL_GPIO_SetPinPull(GPIOC, LL_GPIO_PIN_14, LL_GPIO_PULL_DOWN);
+  LL_GPIO_SetPinSpeed(GPIOC, LL_GPIO_PIN_14, LL_GPIO_SPEED_FREQ_HIGH);
+
+  // PC15
+  LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_15, LL_GPIO_MODE_OUTPUT);
+
+  // Radio Receiver
+  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_INPUT);
 }
 
 void pwm_init(void) {
   LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
 
-    LL_TIM_SetPrescaler(TIM2, 72 - 1); // Prescaler to get 1 MHz timer clock (72 MHz / 72)
-    LL_TIM_SetAutoReload(TIM2, 20000 - 1); // Auto-reload for 20 ms period (50 Hz)
+  LL_TIM_SetPrescaler(
+      TIM2, 72 - 1); // Prescaler to get 1 MHz timer clock (72 MHz / 72)
+  LL_TIM_SetAutoReload(TIM2, 20000 - 1); // Auto-reload for 20 ms period (50 Hz)
 
-	// Channel 1
-    LL_TIM_OC_SetMode(TIM2, LL_TIM_CHANNEL_CH1, LL_TIM_OCMODE_PWM1);
-    LL_TIM_OC_EnablePreload(TIM2, LL_TIM_CHANNEL_CH1);
-	LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
+  // Channel 1
+  LL_TIM_OC_SetMode(TIM2, LL_TIM_CHANNEL_CH1, LL_TIM_OCMODE_PWM1);
+  LL_TIM_OC_EnablePreload(TIM2, LL_TIM_CHANNEL_CH1);
+  LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
 
-	// Channel 2
-	LL_TIM_OC_SetMode(TIM2, LL_TIM_CHANNEL_CH2, LL_TIM_OCMODE_PWM1);
-    LL_TIM_OC_EnablePreload(TIM2, LL_TIM_CHANNEL_CH2);
-	LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH2);
+  // Channel 2
+  LL_TIM_OC_SetMode(TIM2, LL_TIM_CHANNEL_CH2, LL_TIM_OCMODE_PWM1);
+  LL_TIM_OC_EnablePreload(TIM2, LL_TIM_CHANNEL_CH2);
+  LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH2);
 
-
-    LL_TIM_EnableARRPreload(TIM2);
-    LL_TIM_EnableCounter(TIM2);
-    LL_TIM_GenerateEvent_UPDATE(TIM2);
+  LL_TIM_EnableARRPreload(TIM2);
+  LL_TIM_EnableCounter(TIM2);
+  LL_TIM_GenerateEvent_UPDATE(TIM2);
 }
 
 void SystemClock_Config(void) {
   /* Set FLASH latency */
-    LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
-    
-    /* Enable HSE */
-    LL_RCC_HSE_Enable();
-    while (LL_RCC_HSE_IsReady() != 1);
-    
-    /* Configure and enable PLL */
-    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE_DIV_1, LL_RCC_PLL_MUL_9);
-    LL_RCC_PLL_Enable();
-    while (LL_RCC_PLL_IsReady() != 1);
-    
-    /* Set the SYSCLK source to PLL */
-    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
-    
-    /* Set AHB, APB1, and APB2 prescalers */
-    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
-    LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
-    
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
+
+  /* Enable HSE */
+  LL_RCC_HSE_Enable();
+  while (LL_RCC_HSE_IsReady() != 1)
+    ;
+
+  /* Configure and enable PLL */
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE_DIV_1, LL_RCC_PLL_MUL_9);
+  LL_RCC_PLL_Enable();
+  while (LL_RCC_PLL_IsReady() != 1)
+    ;
+
+  /* Set the SYSCLK source to PLL */
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+    ;
+
+  /* Set AHB, APB1, and APB2 prescalers */
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
 }
+
+#define INCREMENT_AMT 200
+#define POSMIN 500
+#define POSMAX 2500
+
+// static int current_pos_tilt = 1500;
+
 void SetServoPosition(int change, uint8_t channel) {
-	switch(channel){
-		case 1:
-			LL_TIM_OC_SetCompareCH1(TIM2, change);
-			break;
-		case 2:
-			LL_TIM_OC_SetCompareCH2(TIM2, change);
-			break;
-		default:
-			break;
-	}
+  switch (channel) {
+  case 1:
+    LL_TIM_OC_SetCompareCH1(TIM2, change);
+    break;
+  case 2:
+    LL_TIM_OC_SetCompareCH2(TIM2, change);
+    break;
+  default:
+    break;
+  }
+}
+
+void Move_Left(void) {
+  static volatile uint32_t current = 1500;
+  LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_13);
+  volatile int new_pos = current - INCREMENT_AMT;
+  if (new_pos < POSMIN) {
+    new_pos = (uint32_t)POSMIN;
+  }
+  current = new_pos;
+  SetServoPosition(current, 1);
+  delay(200);
+  LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_13);
 }
 
 int main(void) {
@@ -96,26 +138,16 @@ int main(void) {
   gpio_init();
   pwm_init();
 
-  int current_pos_1 = 1500;
-  int current_pos_2 = 1500;
-  int posmin = 500;
-  int posmax = 2500;
-
+  //uint8_t gpio14_state = 0;
   while (1) {
-	if(current_pos_1 > posmax){
-		current_pos_1 = posmin;
-	} else {
-		current_pos_1 += 200;
-	}
-
-	if(current_pos_2 < posmin){
-		current_pos_2 = posmax;
-	} else {
-		current_pos_2 -= 200;
-	}
-	SetServoPosition(current_pos_1, 1);
-	delay(200);
-	SetServoPosition(current_pos_2, 2);
-	delay(200);
-  }
+	  /*gpio14_state = LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_14);
+    if (gpio14_state == 1) {
+      Move_Left();
+    }
+ */
+	  SetServoPosition(500, 1);
+	  delay(2000);
+	  SetServoPosition(2500, 1);
+	  delay(2000);
+      }
 }
