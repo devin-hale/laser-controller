2480e13c10ee8cbcedd1361eaccffba69f38d242
Devin Hale <devinwaynehale@gmail.com>
2024-07-17 13:05:33 -0500

    Rolled integer to string function for display

---
+++ inc/utils.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void reverse_str(char* dest, char* str, int length);
+void itos(char* dest, int i);
--- src/utils.c
+++ src/utils.c
@@ -1,23 +1,34 @@
 #include "utils.h"
 
-void itos(int num, char* dest){
-	if (num == 0) {
-		dest = "0";
-		return;
-	}
-	char string[255];
-	int stringPos = 0;
-	while(num != 0){
-		int rem = num % 10;
-		string[stringPos] = rem + '0';
-		num /= 10;
-	}
-	
-	dest[stringPos+1] = '\0';
-	int reversedPos = 0;
-	while(stringPos >= 0){
-		dest[reversedPos] = string[stringPos];
-		reversedPos++;
-		stringPos--;
-	}
+void reverse_str(char* dest, char* str, int length){
+    length--;
+    int i = 0;
+    while(length >= 0){
+        dest[i++] = str[length--];
+    }
+    dest[i] = '\0';
+}
+
+void itos(char* dest, int num){
+    if(num == 0) {
+        dest[0] = '0';
+        dest[1] = '\0';
+        return;
+    }
+    char str[255];
+    int neg = num < 0 ? 1 : 0;
+    if (neg) {
+        num = ~num + 1;
+    }
+    int i = 0;
+    while(num != 0){
+        int rem = num % 10;
+        str[i++] = rem + '0';
+        num /= 10;
+    }
+    if(neg){
+        str[i++] = '-';
+    }
+    str[i] = '\0';
+    reverse_str(dest, str, i);
 }
